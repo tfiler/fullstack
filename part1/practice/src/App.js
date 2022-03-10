86589f800430e071c5c24eff088f9011da796ea8
Trevor <trevorfiler03@gmail.com>
2022-03-10 13:27:57 -0500

    practice for 1b-1d

--- part1/practice/src/App.js
+++ part1/practice/src/App.js
@@ -1,21 +1,37 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [counter, setCounter] = useState(0) //returns value and update func
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () =>  setCounter(0)
+
   return (
     <div>
-      <p>hello {props.name}, you are {props.age} years old</p>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
-  );
-}
-
-const App = () =>  {
-  const name = 'Peter';
-  const age = 10;
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="george" />
-      <Hello name={name} age={age} />
-    </>
-  );
+  )
 }
 
 export default App
