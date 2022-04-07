9adbfdb8b3116cddd2a6008b38b7ac7a49bd83e2
Trevor <trevorfiler03@gmail.com>
2022-04-07 15:13:42 -0400

    Weather now shows wind speed and a conditions icon

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const CountryView = ( {country} ) => {
   const parseLanguages = (country) =>
@@ -17,8 +18,12 @@ const CountryView = ( {country} ) => {
             }
           )}
         </ul>
-      <div><span style={{fontSize: "10rem"}}>{country.flag}</span></div>
       </div>
+      <div>
+        <span style={{fontSize: "10rem"}}>{country.flag}</span>
+      </div>
+      <div><Weather country={country} /></div>
+      
     </div>
   )
 }
@@ -44,6 +49,54 @@ const OptionalView = ( {country, showView} ) => {
   if (showView === true) {
     return(<CountryView country={country} />)
   }
+}
+
+
+const Weather = ({ country }) => {
+  const [weather, setWeather] = useState(['loading weather'])
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY
+    const lat = country.latlng[0]
+    const lon = country.latlng[1]
+   
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [country])
+
+  if (weather[0] === 'loading weather') {
+    return(
+      <div>{weather[0]}</div>
+    )
+  } else {
+    return(
+      <WeatherView weather={weather} />
+    )
+  }
+}
+
+const WeatherView = ({ weather }) => {
+  const mpsToMph = (m) => Math.round(m * 2.2369 * 10) / 10
+  const kToF = (k) => Math.round((1.8 * (k - 273) + 32) * 10) / 10
+
+  const iconCode = weather.weather[0].icon
+  const icon = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+  return (
+    <div>
+      <h3>Weather</h3>
+      <div>Temperature: {kToF(weather.main.temp)}{'\u00B0'} F</div>
+      <div>Conditions: {weather.weather[0].description}</div>
+      <div>
+        <img src={icon} alt="" />
+      </div>
+      <div>Wind: {mpsToMph(weather.wind.speed)} mph</div>
+      <br />
+    </div>
+  )
 }
 
 const CountryList = ( {countries} ) => {
@@ -79,7 +132,6 @@ const Countries = ({ search, allCountries }) => {
   } else {
     return <p>Too many results, please be more specific</p>
   }
-  
 }
 
 export default Countries
